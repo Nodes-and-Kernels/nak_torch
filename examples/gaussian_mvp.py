ff646f048e9e33150d7d8aba9f386c59b55a3d92
Daniel Sharp <dannys4@mit.edu>
2026-04-07 17:32:57 -0400

    Working pyro gaussian

--- examples/gaussian_mvp.py
+++ examples/gaussian_mvp.py
@@ -1,19 +1,22 @@
 # %%
 from functools import partial
 
-from jaxtyping import Float
 import matplotlib.pyplot as plt
 import torch
-
 from torch import Tensor
+from jaxtyping import Float
 
 import nak_torch
 from nak_torch.algorithms import grad_aldi, eks, gradfree_aldi, cbs, msip, kfrflow
-from nak_torch.algorithms.msip import MSIPFredholm, MSIPQuadGradientInformed, MSIPQuadGradientFree
+from nak_torch.algorithms.msip import (
+    MSIPFredholm,
+    MSIPQuadGradientInformed,
+    MSIPQuadGradientFree,
+)
 from nak_torch.tools.quadrature import spherical_MC_radial_Laguerre
 
-from nak_torch.tools.kernel import sqexp_kernel_elem as kernel_elem, sqexp_kernel_matrix
-from tqdm import tqdm
+from pyro.infer import mcmc
+import pyro_tools
 
 if torch.cuda.is_available():
     torch.set_default_device("cuda")
@@ -22,25 +25,23 @@ else:
 
 torch.set_default_dtype(torch.float64)
 
+
 # %%
-
-
 def make_gaussian_post(
     forward_op: Float[Tensor, "obs dim"],
     mean_pr: Float[Tensor, " dim"],
     cov_pr: Float[Tensor, "dim dim"],
     mean_li: Float[Tensor, " obs"],
-    cov_li: Float[Tensor, "obs obs"]
+    cov_li: Float[Tensor, "obs obs"],
 ):
     forward_op = forward_op.T
     cov_post = torch.linalg.inv(
-        forward_op.T @ torch.linalg.solve(
-            cov_li, forward_op
-        ) + torch.linalg.inv(cov_pr)
+        forward_op.T @ torch.linalg.solve(cov_li, forward_op) + torch.linalg.inv(cov_pr)
     )
-    mean_post = cov_post @ (forward_op.T @ torch.linalg.solve(
-        cov_li, mean_li
-    ) + torch.linalg.solve(cov_pr, mean_pr))
+    mean_post = cov_post @ (
+        forward_op.T @ torch.linalg.solve(cov_li, mean_li)
+        + torch.linalg.solve(cov_pr, mean_pr)
+    )
     return mean_post, cov_post
 
 
@@ -54,30 +55,40 @@ def weighted_cov(pts: Tensor, wts: Tensor):
 torch.manual_seed(1023921)
 obs_op = torch.randn(2, 5)
 obs_op.div_(obs_op.norm(dim=1, keepdim=True))
-#forward_model = torch.compile(lambda particles: particles @ obs_op)
-forward_model = lambda particles: particles @ obs_op
+
+
+# forward_model = torch.compile(lambda particles: particles @ obs_op)
+def forward_model(particles, _obs_op=obs_op):
+    return particles @ _obs_op
+
 
 true_obs = torch.tensor([1.0, 2.0, 3.0, 2.0, 1.0]) + 20
 
 model = nak_torch.GaussianModel(
-    forward_model, likelihood_precision=10.0,
-    prior_precision=0.9, true_obs=true_obs,
-    is_vectorized=True
+    forward_model,
+    likelihood_precision=10.0,
+    prior_precision=0.9,
+    true_obs=true_obs,
+    is_vectorized=True,
 )
 
 
-#@torch.compile
+# @torch.compile
 def like_log_dens(pt):
-    ll_term = model.likelihood_precision * \
-        torch.linalg.norm(pt @ obs_op - model.true_obs, dim=-1)**2
+    ll_term = (
+        model.likelihood_precision
+        * torch.linalg.norm(pt @ obs_op - model.true_obs, dim=-1) ** 2
+    )
     return -0.5 * ll_term.squeeze()
 
 
-#@torch.compile
+# @torch.compile
 def post_log_dens(pt):
-    ll_term = model.likelihood_precision * \
-        torch.linalg.norm(pt @ obs_op - model.true_obs, dim=-1)**2
-    prior_term = model.prior_precision * torch.linalg.norm(pt, dim=-1)**2
+    ll_term = (
+        model.likelihood_precision
+        * torch.linalg.norm(pt @ obs_op - model.true_obs, dim=-1) ** 2
+    )
+    prior_term = model.prior_precision * torch.linalg.norm(pt, dim=-1) ** 2
     return -0.5 * (ll_term + prior_term).squeeze()
 
 
@@ -87,13 +98,12 @@ post_log_dens_grad_val_batch = torch.vmap(post_log_dens_grad_val)
 
 # %%
 mean_pr, cov_pr = torch.zeros(2), torch.eye(2) / model.prior_precision
-mean_li, cov_li = model.true_obs, torch.eye(
-    len(model.true_obs)
-) / model.likelihood_precision
-
-mean_post, cov_post = make_gaussian_post(
-    obs_op, mean_pr, cov_pr, mean_li, cov_li
+mean_li, cov_li = (
+    model.true_obs,
+    torch.eye(len(model.true_obs)) / model.likelihood_precision,
 )
+
+mean_post, cov_post = make_gaussian_post(obs_op, mean_pr, cov_pr, mean_li, cov_li)
 vals, vecs = torch.linalg.eigh(cov_post)
 cov_post_sqrt = vecs @ torch.diag(torch.sqrt(vals)) @ vecs.T
 samps = torch.randn(10000, 2) @ cov_post_sqrt + mean_post
@@ -103,84 +113,118 @@ n_steps, n_particles = 50000, 50
 lr = 0.1
 
 # %% Initialization
-
-init_particles = torch.randn((n_particles, 2)) / \
-    model.prior_precision + torch.tensor([3.2,-5.0])
+init_particles = torch.randn((n_particles, 2)) / model.prior_precision + torch.tensor(
+    [3.2, -5.0]
+)
 
 # init_particles = torch.randn((n_particles, 2)) + torch.tensor([3, -3])
-#torch.randn((n_particles_kfr,2)) + torch.tensor([3,-5])
+# torch.randn((n_particles_kfr,2)) + torch.tensor([3,-5])
 # %% EKS
 trajectories_eks = eks(
-    model, n_particles=n_particles,
-    n_steps=n_steps, dim=2, lr=lr,
+    model,
+    n_particles=n_particles,
+    n_steps=n_steps,
+    dim=2,
+    lr=lr,
     init_particles=init_particles,
-    keep_all=False, compile_step=False,
-    verbose=True
+    keep_all=False,
+    compile_step=False,
+    verbose=True,
 )
+
+# %%
+n_steps_hmc = n_steps - 100
+pyro_model = pyro_tools.PyroModel(model, 2)
+hmc_kernel = mcmc.NUTS(pyro_model)
+mcmc_setup = mcmc.MCMC(hmc_kernel, num_samples=n_steps_hmc, warmup_steps=100)
+mcmc_setup.run(model.true_obs)
+
+# %%
+hmc_samples = mcmc_setup.get_samples()["theta"]
+
+plt.scatter(hmc_samples[:, 0], hmc_samples[:, 1])
 
 # %% KFR
 
 # delta_ts = torch.ones(1000)/1000
-#def imq(pt1,pt2,h):
+# def imq(pt1,pt2,h):
 #    return 1/torch.sqrt(1 + (torch.linalg.norm(pt1-pt2) / h)**2)
 
 trajectories_kfr = kfrflow(
     like_log_dens,
     n_particles,
-    n_steps, 2,
+    n_steps,
+    2,
     init_particles=init_particles,
-    kernel_length_scale = 1e-2,
+    kernel_length_scale=1e-2,
     kernel_diag_infl=1e-5,
     # bounds=(-10,10),
     # kernel_elem=imq,
     keep_all=False,
     compile_step=False,
-    verbose = True
+    verbose=True,
 )
 
 
 # %% GI-ALDI
 
 trajectories_galdi = grad_aldi(
-    post_log_dens, n_particles, n_steps, dim=2,
-    lr=lr, init_particles=init_particles,
-    keep_all=False, compile_step=False,
+    post_log_dens,
+    n_particles,
+    n_steps,
+    dim=2,
+    lr=lr,
+    init_particles=init_particles,
+    keep_all=False,
+    compile_step=False,
     verbose=True,
 )
 
 # %% GF-ALDI
 
 trajectories_gfaldi = gradfree_aldi(
-    model, n_particles, n_steps, dim=2,
-    lr=lr, init_particles=init_particles,
-    keep_all=True, compile_step=False,
-    verbose=True
+    model,
+    n_particles,
+    n_steps,
+    dim=2,
+    lr=lr,
+    init_particles=init_particles,
+    keep_all=True,
+    compile_step=False,
+    verbose=True,
 )
 
 # %% CBS
 
 trajectories_cbs = cbs(
-    post_log_dens, n_particles, n_steps, inverse_temp=0.95, dim=2,
-    lr=lr, init_particles=init_particles,
-    keep_all=True, compile_step=False,
-    verbose=True
+    post_log_dens,
+    n_particles,
+    n_steps,
+    inverse_temp=0.95,
+    dim=2,
+    lr=lr,
+    init_particles=init_particles,
+    keep_all=True,
+    compile_step=False,
+    verbose=True,
 )
 
 # %% F-MSIP
 
 kernel_length_scale = 0.03
-bounds = (-100., 100.)
+bounds = (-100.0, 100.0)
 gradient_decay = 1.0
 lr_msip = 100e-2
 kernel_diag_infl = 1e-8
-msip_fredholm = MSIPFredholm(
-    gradient_decay,
-    post_log_dens_grad_val_batch
-)
+msip_fredholm = MSIPFredholm(gradient_decay, post_log_dens_grad_val_batch)
 
 trajectories_msip, traj_wts_msip = msip(
-    msip_fredholm, n_particles, n_steps, dim=2,
-    lr=lr_msip, init_particles=init_particles[:n_particles],
+    msip_fredholm,
+    n_particles,
+    n_steps,
+    dim=2,
+    lr=lr_msip,
+    init_particles=init_particles[:n_particles],
     kernel_length_scale=kernel_length_scale,
     is_log_density_batched=True,
     kernel_diag_infl=kernel_diag_infl,
@@ -188,7 +232,7 @@ trajectories_msip, traj_wts_msip = msip(
     gradient_decay=gradient_decay,
     keep_all=True,
     compile_step=False,
-    verbose=True
+    verbose=True,
 )
 
 # %%
@@ -201,8 +245,7 @@ def mc_quad_rule(batch_size: int, N_quad: int = 5, dim: int = 2):
 
 
 def spherical_quad(batch_size: int, N_spherical: int = 5, N_radial: int = 3):
-    pts, wts = spherical_MC_radial_Laguerre(
-        batch_size, N_spherical, 2, N_radial)
+    pts, wts = spherical_MC_radial_Laguerre(batch_size, N_spherical, 2, N_radial)
     return pts, wts
 
 
@@ -210,13 +253,16 @@ def spherical_quad(batch_size: int, N_spherical: int = 5, N_radial: int = 3):
 # kernel_length_scale = 1e-3
 # gradient_decay = 1.
 msip_quadgrad = MSIPQuadGradientInformed(
-    post_log_dens_grad_val_batch, mc_quad_rule,
-    gradient_decay
+    post_log_dens_grad_val_batch, mc_quad_rule, gradient_decay
 )
 
 trajectories_msip_qg, traj_wts_msip_qg = msip(
-    msip_quadgrad, n_particles, n_steps, dim=2,
-    lr=10., init_particles=init_particles[:n_particles],
+    msip_quadgrad,
+    n_particles,
+    n_steps,
+    dim=2,
+    lr=10.0,
+    init_particles=init_particles[:n_particles],
     kernel_length_scale=kernel_length_scale,
     # is_log_density_batched=True,
     kernel_diag_infl=1e-8,
@@ -224,7 +270,7 @@ trajectories_msip_qg, traj_wts_msip_qg = msip(
     # gradient_decay=gradient_decay,
     keep_all=False,
     compile_step=False,
-    verbose=True
+    verbose=True,
 )
 
 # %%
@@ -235,19 +281,24 @@ msip_quadgf = MSIPQuadGradientFree(
 )
 
 trajectories_msip_qgf, traj_wts_msip_qgf = msip(
-    msip_quadgf, n_particles, n_steps, dim=2,
-    lr=1., init_particles=init_particles[:n_particles],
+    msip_quadgf,
+    n_particles,
+    n_steps,
+    dim=2,
+    lr=1.0,
+    init_particles=init_particles[:n_particles],
     kernel_length_scale=kernel_length_scale,
     kernel_diag_infl=1e-8,
-    bounds=(-1000., 1000.),
-    keep_all=False, compile_step=False,
-    verbose=True
+    bounds=(-1000.0, 1000.0),
+    keep_all=False,
+    compile_step=False,
+    verbose=True,
 )
 
 
 # %%
 pts_eks = trajectories_eks[-1]
-#pts_kfr = particles_kfr
+# pts_kfr = particles_kfr
 pts_galdi = trajectories_galdi[-1]
 pts_gfaldi = trajectories_gfaldi[-1]
 pts_cbs = trajectories_cbs[-1]
@@ -257,7 +308,7 @@ wts_msip = traj_wts_msip[idx_msip]
 # wts_msip /= wts_msip.sum()
 pts_msip_qg = trajectories_msip_qg[-1]
 wts_msip_qg = traj_wts_msip_qg[-1]
-wts_msip_qg = wts_msip_qg/wts_msip_qg.sum()
+wts_msip_qg = wts_msip_qg / wts_msip_qg.sum()
 pts_msip_qgf = trajectories_msip_qgf[-1]
 wts_msip_qgf = traj_wts_msip_qgf[-1]
 # wts_msip_qgf = wts_msip_qgf/wts_msip_qgf.sum()
@@ -312,27 +363,24 @@ grid_pts = torch.stack((X.flatten(), Y.flatten()), 1)
 # plt.show()
 
 
-
 fig, ax = plt.subplots()
 ax.contour(X, Y, post_log_dens(grid_pts).reshape(Ngrid, Ngrid), levels=10)
 # ax.scatter(samps[:, 0], samps[:, 1], alpha=0.025, label="Truth")
 # ax.scatter(pts_galdi[:, 0], pts_galdi[:, 1], alpha=0.2, label="Grad-ALDI")
 # ax.scatter(pts_gfaldi[:, 0], pts_gfaldi[:, 1],
 #            alpha=0.2, label="GradFree-ALDI")
-#ax.scatter(pts_kfr[:,0], pts_kfr[:,1], label="KFR")
+# ax.scatter(pts_kfr[:,0], pts_kfr[:,1], label="KFR")
 # ax.scatter(pts_eks[:, 0], pts_eks[:, 1], alpha=0.1, label="EKS")
 # ax.scatter(pts_cbs[:, 0], pts_cbs[:, 1], alpha=0.1, label="CBS")
-s = ax.scatter(pts_msip[:, 0], pts_msip[:, 1],
-                c=wts_msip, alpha=0.15, label="MSIP")
-#s = ax.scatter(pts_msip_qg[:, 0], pts_msip_qg[:, 1],
+s = ax.scatter(pts_msip[:, 0], pts_msip[:, 1], c=wts_msip, alpha=0.15, label="MSIP")
+# s = ax.scatter(pts_msip_qg[:, 0], pts_msip_qg[:, 1],
 #                c = wts_msip_qg, alpha=0.15, label="MSIP-QuadGrad")
-#s = ax.scatter(pts_msip_qgf[:, 0], pts_msip_qgf[:, 1],
+# s = ax.scatter(pts_msip_qgf[:, 0], pts_msip_qgf[:, 1],
 #                c = wts_msip_qgf, alpha=0.15, label="MSIP-QuadGradFree")
 # plt.colorbar(s)
 ax.set_aspect(1.0)
 ax.legend()
 plt.show()
-
 
 
 fig, ax = plt.subplots()
@@ -341,20 +389,19 @@ ax.contour(X, Y, post_log_dens(grid_pts).reshape(Ngrid, Ngrid), levels=10)
 ax.scatter(pts_galdi[:, 0], pts_galdi[:, 1], alpha=0.2, label="Grad-ALDI")
 # ax.scatter(pts_gfaldi[:, 0], pts_gfaldi[:, 1],
 #            alpha=0.2, label="GradFree-ALDI")
-#ax.scatter(pts_kfr[:,0], pts_kfr[:,1], label="KFR")
+# ax.scatter(pts_kfr[:,0], pts_kfr[:,1], label="KFR")
 # ax.scatter(pts_eks[:, 0], pts_eks[:, 1], alpha=0.1, label="EKS")
 # ax.scatter(pts_cbs[:, 0], pts_cbs[:, 1], alpha=0.1, label="CBS")
-#s = ax.scatter(pts_msip[:, 0], pts_msip[:, 1],
+# s = ax.scatter(pts_msip[:, 0], pts_msip[:, 1],
 #                c=wts_msip, alpha=0.15, label="MSIP")
-#s = ax.scatter(pts_msip_qg[:, 0], pts_msip_qg[:, 1],
+# s = ax.scatter(pts_msip_qg[:, 0], pts_msip_qg[:, 1],
 #                c = wts_msip_qg, alpha=0.15, label="MSIP-QuadGrad")
-#s = ax.scatter(pts_msip_qgf[:, 0], pts_msip_qgf[:, 1],
+# s = ax.scatter(pts_msip_qgf[:, 0], pts_msip_qgf[:, 1],
 #                c = wts_msip_qgf, alpha=0.15, label="MSIP-QuadGradFree")
 # plt.colorbar(s)
 ax.set_aspect(1.0)
 ax.legend()
 plt.show()
-
 
 
 # %%
