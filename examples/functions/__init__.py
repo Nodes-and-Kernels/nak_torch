a4d9d08e6127be9121396f32ea5754c8aaa1cbe5
Daniel Sharp <dannys4@mit.edu>
2026-08-11 16:02:32 -0400

    clean up old examples

--- examples/functions/__init__.py
+++ examples/functions/__init__.py
@@ -8,19 +8,11 @@
 # 05/12/2025
 
 
-from .twodims.mixture_of_gaussians import mixture_of_gaussians
-from .twodims.himmelblau import himmelblau
-from . import banana
-from .nns.shallow_nn_twodims import loss_nn_dataset
-from .kde.gaussian_kde import gaussian_kde
+from . import banana, himmelblau
 from .aristoff_bangerth import build_aristoff_bangerth
 
 __all__ = [
-    "mixture_of_gaussians",
     "banana",
     "himmelblau",
-    "loss_nn_dataset",
-    # "plot_2D_classification_with_dataset_from_theta",
-    "gaussian_kde",
     "build_aristoff_bangerth"
 ]
